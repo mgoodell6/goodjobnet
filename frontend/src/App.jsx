2dbe7315dd8fdc0b18aa60bf20fc60b10044d216
mgood <mgoodell6@yahoo.com>
2026-06-12 10:18:14 -0400

    Created new local_helper to address issues when using Voice Assistant and Google Voice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,11 @@ import JobSeekerDashboard from './pages/JobSeekerDashboard';
 import JobSeekerMatchesReport from './pages/JobSeekerMatchesReport';
 
 // Manual Version Configuration - Update this string to change the application version displayed in the header
+<<<<<<< Updated upstream
 const APP_VERSION = "Beta v0.11";
+=======
+const APP_VERSION = "Beta v0.13";
+>>>>>>> Stashed changes
 
 function TopBar({ user, handleLogout }) {
   const location = useLocation();
